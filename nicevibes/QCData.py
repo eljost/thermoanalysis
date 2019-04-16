further work and added some log files. right now there seems to be a bug in the calculation of the rotational entropy

nicevibes/QCData.py
@@ -3,6 +3,9 @@
 import re
 
 import cclib
+import numpy as np
+
+from nicevibes.constants import C, ANG2M, AMU2KG, PLANCK, KB
 
 class QCData:
 
@@ -10,29 +13,55 @@ class QCData:
                  point_group="c1", scale_factor=1.0, solvent=None):
         self.log_fn = log_fn
 
-        self.point_group = point_group
+        self.point_group = point_group.lower()
         self.scale_factor = scale_factor
         self.solvent = solvent
 
         self.symmetry_number = self.get_symmetry_number()
 
-        parser = cclib.io.ccopen(self.log_fn)
+        self.set_data(self.log_fn)
+
+    def set_data(self, log_fn):
+        parser = cclib.io.ccopen(log_fn)
         data = parser.parse()
         self.data = data
-        self.vibfreqs = self.scale_factor * self.data.vibfreqs
+        coords3d = self.data.atomcoords# * ANG2AU
+        assert coords3d.shape[0] == 1
+        self.coords3d = coords3d[0]
+        self.wavenumbers = self.scale_factor * self.data.vibfreqs
         self.scf_energy = self.data.scfenergies[-1]
-        self.atom_masses = self.data.atommasses
+        self.masses = self.data.atommasses
         self._mult = self.data.mult
 
     @property
     def M(self):
         """Molecular mass."""
-        return self.atom_masses.sum()
+        return self.masses.sum()
 
     @property
     def mult(self):
         """Multiplicity."""
         return self._mult
+
+    @property
+    def vib_frequencies(self):
+        return C * self.wavenumbers * 100
+
+    @property
+    def is_linear(self):
+        return self.point_group in ("cinf", "dinfh")
+    
+    @property
+    def is_atom(self):
+        return len(self.masses) == 1
+
+    @property
+    def rot_temperatures(self):
+        self.standard_orientation()
+        I = self.inertia_tensor() * ANG2M**2 * AMU2KG
+        w, v = np.linalg.eigh(I)
+        rot_temps = PLANCK**2/(8*np.pi**2*w*KB)
+        return rot_temps
 
     def get_symmetry_number(self, point_group=None):
         symm_dict = {
@@ -68,3 +97,69 @@ class QCData:
             "specify some 'Sn' group with n ∈ (1, 3, 5, ...)? Please use " \
             "the corresponding 'Cnm' groups instead!"
         return sym_num
+
+    def inertia_tensor(self):
+        """Inertita tensor.
+
+                              | x² xy xz |
+        (x y z)^T . (x y z) = | xy y² yz |
+                              | xz yz z² |
+        """
+        x, y, z = self.coords3d.T
+        squares = np.sum(self.coords3d**2 * self.masses[:,None], axis=0)
+        I_xx = squares[1] + squares[2]
+        I_yy = squares[0] + squares[2]
+        I_zz = squares[0] + squares[1]
+        I_xy = -np.sum(self.masses*x*y)
+        I_xz = -np.sum(self.masses*x*z)
+        I_yz = -np.sum(self.masses*y*z)
+        I = np.array((
+                (I_xx, I_xy, I_xz),
+                (I_xy, I_yy, I_yz),
+                (I_xz, I_yz, I_zz)
+        ))
+        return I
+
+    @property
+    def center_of_mass(self):
+        """Returns the center of mass.
+
+        Returns
+        -------
+        R : np.array, shape(3, )
+            Center of mass.
+        """
+        return 1/self.M * np.sum(self.coords3d*self.masses[:,None],
+                                 axis=0)
+
+    def principal_axes_are_aligned(self):
+        """Check if the principal axes are aligned with the cartesian axes.
+
+        Returns
+        -------
+        aligned : bool
+            Wether the principal axes are aligned or not.
+        """
+        w, v = np.linalg.eigh(self.inertia_tensor())
+        return np.allclose(v, np.eye(3)), v
+
+    def align_principal_axes(self):
+        """Align the principal axes to the cartesian axes.
+
+        https://math.stackexchange.com/questions/145023
+        """
+        I = self.inertia_tensor()
+        w, v = np.linalg.eigh(I)
+        # rot = np.linalg.solve(v, np.eye(3))
+        # self.coords3d = rot.dot(self.coords3d.T).T
+        self.coords3d = v.T.dot(self.coords3d.T).T
+
+    def standard_orientation(self):
+        # Translate center of mass to cartesian origin
+        self.coords3d -= self.center_of_mass
+        # Try to rotate the principal axes onto the cartesian axes
+        for i in range(5):
+            self.align_principal_axes()
+            aligned, vecs = self.principal_axes_are_aligned()
+            if aligned:
+                break

nicevibes/main.py
@@ -8,7 +8,7 @@ from pysisyphus.helpers import geom_from_xyz_file
 from pysisyphus.Geometry import Geometry
 
 
-from nicevibes.constants import C, KB, NA, R, PLANCK
+from nicevibes.constants import C, KB, NA, R, PLANCK, J2AU
 
 
 def rotational_temperature(Rm):
@@ -35,16 +35,17 @@ def get_V_free(solvent="chloroform", C_free=8):
 
 def translation_energy(temperature):
     """Kinectic energy of an ideal gas."""
-    return 3/2 * R * temperature
+    return 3/2 * KB * temperature
 
 
 def electronic_entropy(multiplicity):
     """Considering only the ground state."""
-    return R * math.log(multiplicity)
+    return KB * math.log(multiplicity)
 
 
 def sackur_tetrode(molecular_mass, temperature):
     """Translational entropy for a monoatomic ideal gas."""
+    print("Wrong unit in sackur tetrode! Convert to single particle energy!")
     return (  3/2 * R * np.log(molecular_mass)
             + 5/2 * R * np.log(temperature)
             - 2.315)
@@ -57,13 +58,61 @@ def translational_entropy(molecular_mass, temperature, kind="sackur"):
     return funcs[kind](molecular_mass, temperature)
 
 
+def zero_point_energy(frequencies):
+    return (PLANCK * frequencies  / 2).sum()
+
+
+def rotational_energy(temperature, is_linear, is_atom):
+    if is_atom:
+        rot_energy = 0
+    elif is_linear:
+        rot_energy = KB * temperature
+
+    rot_energy = 3/2 * KB * temperature
+    return rot_energy
+
+
+def vibrational_energy(temperature, frequencies):
+    vib_temperatures = PLANCK * frequencies / KB
+    return KB * np.sum(vib_temperatures
+                       * (1/2 + 1 / (np.exp(vib_temperatures/temperature) - 1))
+    )
+
+
+def rotational_entropy(temperature, rot_temperatures, symmetry_number,
+                       is_linear, is_atom):
+    if is_atom:
+        S_rot = 0
+    q_rot = KB * temperature / (rot_temperatures * symmetry_number)
+    if is_linear:
+        S_rot = KB * (np.log(q_rot) + 1)
+    S_rot = KB * (np.log(q_rot) + 3/2)
+    return S_rot.sum()
+
+
 def thermochemistry(qc, temperature):
+    J2au = lambda J: f"{J*J2AU:.8f} au"
+
+    zpe = zero_point_energy(qc.vib_frequencies)
+    print("ZPE", zpe, "J", f"{zpe*J2AU:.6f} au")
     U_trans = translation_energy(temperature)
-    print("U_trans", U_trans)
+    print("U_trans", U_trans, J2au(U_trans))
+    U_rot = rotational_energy(temperature, qc.is_linear, qc.is_atom)
+    print("U_rot", U_rot, J2au(U_rot))
+    U_vib = vibrational_energy(temperature, qc.vib_frequencies)
+    print("U_vib", U_vib, J2au(U_vib))
+
+    # ZPE isn't included here as it is already included in the U_vib term
+    therm_corr = U_rot + U_vib + U_trans
+    print("thermal_corr", therm_corr, J2au(therm_corr))
+
     S_el = electronic_entropy(qc.mult)
     print("S_el", S_el)
     S_trans = translational_entropy(qc.M, temperature)
     print("S_trans", S_trans)
+    S_rot = rotational_entropy(temperature, qc.rot_temperatures, qc.symmetry_number,
+                               qc.is_linear, qc.is_atom)
+    print("S_rot", S_rot, J2au(S_rot))
 
 
 def run():

nicevibes/tests/playground.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
 from nicevibes.QCData import QCData
+from nicevibes.constants import PLANCK, KB, AMU2KG
+
+
+np.set_printoptions(suppress=True, precision=4)
 
 
 def run():
-    log = "logs/02_dmso_freq.log"
+    # log = "logs/02_dmso_pm6_freq.log"
+    log = "logs/04_dmso_hf_freq.log"
     q = QCData(log, point_group="c2v")
-    # import pdb; pdb.set_trace()
     print(q)
+    rt = q.rot_temperatures
+    print("rot_temps", rt)
 
     temp = 298.15
 