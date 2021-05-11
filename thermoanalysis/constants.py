73fd5d8f892b392882c13a8ed328bb438d910237
Johannes Steinmetzer <josteinm@posteo.de>
2021-05-11 11:03:47 +0200

    switched manually defined units to scipy.constants

--- thermoanalysis/constants.py
+++ thermoanalysis/constants.py
@@ -1,24 +1,20 @@
-#!/usr/bin/env python3
+import scipy.constants as spc
 
-# Just for reference, these values are right now not used here:
-#     http://gaussian.com/constants/
 
-AU2EV = 27.21138602 # Hartree to eV
-CAL2J = 4.1868
-J2CAL = 1/CAL2J # Joule to calorie
-J2AU = 2.293710449e+17 # Joule to Hartree
-ANG2AU = 1.8897259886 # Angstrom to Bohr
-AMU2KG = 1.660539e-27 # Atomic mass units to g
-ANG2M = 1e-10 # Angstrom to meter
-
-R = 8.3144598 # J/(K*mol), ideal gas constant
-C = 299_792_458 # m/s, speed of light
-PLANCK = 6.626_0700_40e-34 # J/s, Planck constant
-KB = 1.380_648_52e-23 # J/K, Boltzmann constant
+AU2EV = spc.value("Hartree energy in eV")  # Hartree to eV
+CAL2J = spc.calorie
+J2CAL = 1 / CAL2J  # Joule to calorie
+J2AU = 1 / spc.value("Hartree energy")  # Joule to Hartree
+ANG2AU = 1 / spc.value("Bohr radius") / 1e10  # Angstrom to Bohr
+AMU2KG = spc.value("unified atomic mass unit")  # Atomic mass units to g
+ANG2M = 1e-10  # Angstrom to meter
+R = spc.R  # J/(K*mol), ideal gas constant
+C = spc.c  # Speed of light in m/s
+PLANCK = spc.Planck  # J/s, Planck constant
+KB = spc.Boltzmann  # J/K, Boltzmann constant
 KBAU = KB * J2AU
-NA = 6.022_140_857e23 # 1/mol, Avogadro constant
-
-AU2J_MOL = 1/J2AU*NA
+NA = spc.N_A  # 1/mol, Avogadro constant
+AU2J_MOL = 1 / J2AU * NA
 KCAL2J = CAL2J * 1000
 CAL_MOL2AU = CAL2J * J2AU / NA
 KCAL_MOL2AU = 1000 * CAL_MOL2AU
