9fe8b6ac2bf9f52e5cf62890fd2b0481771b2fa2
Johannes Steinmetzer <josteinm@posteo.de>
2022-03-07 16:25:47 +0100

    add: partition functions

--- thermoanalysis/thermo.py
+++ thermoanalysis/thermo.py
@@ -22,7 +22,7 @@ from thermoanalysis.constants import (
     J2CAL,
     AMU2KG,
 )
-from thermoanalysis.config import ROTOR_CUT_DEFAULT
+from thermoanalysis.config import p_DEFAULT, ROTOR_CUT_DEFAULT
 
 
 ThermoResults = namedtuple(
@@ -43,10 +43,35 @@ ThermoResults = namedtuple(
 ##############
 
 
+def electronic_part_func(multiplicity, electronic_energies, temperature):
+    """Electronic partition function.
+
+    Parameters
+    ----------
+    multiplicity : int
+        Multiplicity of the molecule.
+    electronic_energies : float
+        Electronic energy in Hartree.
+    temperature : float
+        Absolute temperature in Kelvin.
+
+    Returns
+    -------
+    Q_el : float
+        Electronic partition function.
+    """
+
+    electronic_energies = np.atleast_1d(electronic_energies)
+    energy_diffs = electronic_energies - electronic_energies.min()
+    g = multiplicity
+    q_el = g * (np.exp(-energy_diffs / (KBAU * temperature))).sum()
+    return q_el
+
+
 def electronic_entropy(multiplicity):
     """Electronic entropy.
 
-    Only the ground state is considered. See [1] for reference.
+    Currently, only one electronic state is considered. See [1] for reference.
 
     Parameters
     ----------
@@ -67,7 +92,33 @@ def electronic_entropy(multiplicity):
 #######################
 
 
-def translation_energy(temperature):
+def translational_part_func(molecular_mass, temperature, pressure):
+    """Translational partition function Q_trans.
+
+    Parameters
+    ----------
+    molecular_mass : float
+        Molecular mass in atomic mass units (amu).
+    temperature : float
+        Absolute temperature in Kelvin.
+    pressure : float, optional
+        Pressure in Pascal.
+
+    Returns
+    -------
+    q_trans : float
+        Translational partition function.
+    """
+    # Volume of an atom of an ideal gas
+    volume = KB * temperature / pressure
+    molecular_mass_kg = molecular_mass * AMU2KG
+    q_trans = (
+        (2 * np.pi * molecular_mass_kg * KB * temperature / PLANCK**2) ** 1.5
+    ) * volume
+    return q_trans
+
+
+def translational_energy(temperature):
     """Kinectic energy of an ideal gas.
 
     See [1] for reference.
@@ -86,7 +137,7 @@ def translation_energy(temperature):
     return U_trans
 
 
-def sackur_tetrode(molecular_mass, temperature, pressure=1e5):
+def sackur_tetrode(molecular_mass, temperature, pressure=p_DEFAULT):
     """Translational entropy of an ideal gas.
 
     See [1] for reference.
@@ -107,9 +158,8 @@ def sackur_tetrode(molecular_mass, temperature, pressure=1e5):
     """
     # Just using 1e5 instead of a "true" atmosphere of 1.01325e5 seems to
     # agree better with the results Gaussian and ORCA produce.
-    # pressure = 1.01325e5
     q_trans = (
-        (2 * np.pi * molecular_mass * AMU2KG * KB * temperature / PLANCK ** 2)
+        (2 * np.pi * molecular_mass * AMU2KG * KB * temperature / PLANCK**2)
         ** (3 / 2)
         * KB
         * temperature
@@ -173,6 +223,47 @@ def translational_entropy(molecular_mass, temperature, pressure, kind="sackur"):
 ####################
 
 
+def rotational_part_func(
+    temperature, rot_temperatures, symmetry_number, is_atom, is_linear=False
+):
+    """Rotational partition function Q_rot.
+
+    See [1] for reference.
+
+    Parameters
+    ----------
+    temperature : float
+        Absolute temperature in Kelvin.
+    rot_temperatures : np.array of size 3
+        Rotational temperatures in Kelvin.
+    symmetry_number : int
+        Symmetry number.
+    is_atom : bool
+        Wether the molcule is an atom.
+    is_linear : bool, optional
+        Wether the molecule is linear.
+
+    Returns
+    -------
+    Q_rot : float
+        Rotational partition function.
+    """
+    if is_atom:
+        q_rot = 1
+    elif is_linear:
+        # First rot_temperature will be infinite, and the last two components
+        # will be identical. Only use the last one.
+        q_rot = temperature / (rot_temperatures[-1] * symmetry_number)
+    else:
+        # Polyamtomic, non-linear case
+        q_rot = (
+            np.pi ** (1 / 2)
+            / symmetry_number
+            * (temperature ** (3 / 2) / np.product(rot_temperatures) ** (1 / 2))
+        )
+    return q_rot
+
+
 def rotational_energy(temperature, is_linear, is_atom):
     """Rotational energy.
 
@@ -204,7 +295,7 @@ def rotational_energy(temperature, is_linear, is_atom):
 
 
 def rotational_entropy(
-    temperature, rot_temperatures, symmetry_number, is_linear, is_atom
+    temperature, rot_temperatures, symmetry_number, is_atom, is_linear=False
 ):
     """Rotational entropy.
 
@@ -218,31 +309,28 @@ def rotational_entropy(
         Rotational temperatures in Kelvin.
     symmetry_number : int
         Symmetry number.
-    is_linear : bool
-        Wether the molecule is linear.
     is_atom : bool
         Wether the molcule is an atom.
+    is_linear : bool, optional
+        Wether the molecule is linear.
 
     Returns
     -------
     S_rot : float
         Rotational entropy in Hartree /(particle * K).
     """
+
+    q_rot = rotational_part_func(
+        temperature, rot_temperatures, symmetry_number, is_atom, is_linear=is_linear
+    )
     if is_atom:
-        S_rot = 0
+        plus = 0
     elif is_linear:
-        # First rot_temperature will be infinite, and the last two components
-        # will be identical. Only use the last one.
-        q_rot = temperature / (rot_temperatures[-1] * symmetry_number)
-        S_rot = KBAU * (np.log(q_rot) + 1)
+        plus = 1
+    # Polyamtomic, non-linear case
     else:
-        # Polyamtomic, non-linear case
-        q_rot = (
-            np.pi ** (1 / 2)
-            / symmetry_number
-            * (temperature ** (3 / 2) / np.product(rot_temperatures) ** (1 / 2))
-        )
-        S_rot = KBAU * (np.log(q_rot) + 3 / 2)
+        plus = 1.5  # 3 / 2
+    S_rot = KBAU * (np.log(q_rot) + plus)
     return S_rot
 
 
@@ -269,6 +357,30 @@ def zero_point_energy(frequencies):
 
     ZPE = J2AU * (PLANCK * frequencies / 2).sum()
     return ZPE
+
+
+def vibrational_part_func(temperature, frequencies):
+    """Vibrational partition function.
+
+    Parameters
+    ----------
+    temperature : float
+        Absolute temperature in Kelvin.
+    frequencies : array-like
+        Vibrational frequencies in 1/s.
+
+    Returns
+    -------
+    q_vib : float
+        Vibrational partition function.
+    """
+    frequencies = np.array(frequencies, dtype=float)
+    quot = -PLANCK * frequencies / (KB * temperature)
+    quot_half = quot / 2
+    q_vib = np.product(
+        np.exp(quot_half) / (1 - np.exp(quot))
+    )
+    return q_vib
 
 
 def vibrational_energy(temperature, frequencies):
@@ -351,12 +463,12 @@ def free_rotor_entropies(temperature, frequencies, B_av=1e-44):
     S_free_rots : array-like
         Array containing free-rotor entropies in Hartree / (particle * K).
     """
-    inertia_moments = PLANCK / (8 * np.pi ** 2 * frequencies)
+    inertia_moments = PLANCK / (8 * np.pi**2 * frequencies)
     eff_inertia_moments = (inertia_moments * B_av) / (inertia_moments + B_av)
     S_free_rots = KBAU * (
         1 / 2
         + np.log(
-            (8 * np.pi ** 3 * eff_inertia_moments * KB * temperature / PLANCK ** 2)
+            (8 * np.pi**3 * eff_inertia_moments * KB * temperature / PLANCK**2)
             ** (1 / 2)
         )
     )
@@ -421,7 +533,7 @@ def vibrational_entropy(temperature, frequencies, cutoff=100, alpha=4):
 def thermochemistry(
     qc,
     temperature,
-    pressure=1e5,
+    pressure=p_DEFAULT,
     kind="qrrho",
     rotor_cutoff=ROTOR_CUT_DEFAULT,
     scale_factor=1.0,
@@ -476,7 +588,7 @@ def thermochemistry(
     vib_frequencies = C * wavenumbers * 100
 
     U_el = qc.scf_energy
-    U_trans = translation_energy(T)
+    U_trans = translational_energy(T)
     U_rot = rotational_energy(T, qc.is_linear, qc.is_atom)
     U_vib = vibrational_energy(T, vib_frequencies)
 
